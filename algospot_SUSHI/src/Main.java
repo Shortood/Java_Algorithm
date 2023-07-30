393c6fa9be248afe7ae986f0e89b503be86657ef
Kang Woobin <woobin929@dgu.ac.kr>
2023-07-31 00:26:28 +0900

    algospot_SUSHI 성공

--- algospot_SUSHI/src/Main.java
+++ algospot_SUSHI/src/Main.java
@@ -5,46 +5,36 @@ public class Main {
     static int c, n, m;
     static int[] dp = new int[201];
     static int result = 0;
-
-    static Vector<Pair> sushi = new Vector<Pair>();
-
-    static class Pair {
-        int price, prefer;
-
-        public Pair(int price, int prefer) {
-            this.price = price;
-            this.prefer = prefer;
-        }
-
-    }
+    public static int[] price = new int[20];
+    public static int[] prefer = new int[20];
 
     public static int DP() {
+        dp[0] = 0;
         for (int i = 1; i <= m; i++) { //가격
             int sel = 0;
             for (int j = 0; j < n; j++) {
-                if (i >= sushi.get(j).price)
-                    sel = Math.max(dp[(i - sushi.get(j).price) % 201] + sushi.get(j).price, sel);
+                if (i >= price[j])
+                    sel = Math.max(dp[(i - price[j]) % 201] + prefer[j], sel);
             }
             dp[i % 201] = sel;
+            result = Math.max(result, sel);
         }
-        return dp[m % 201];
+        return result;
     }
 
 
     public static void main(String[] args) {
-        int c1, c2;
         Scanner sc = new Scanner(System.in);
         c = sc.nextInt(); //테스트 케이스 수
         for (int p = 0; p < c; p++) {
+            result = 0;
             n = sc.nextInt(); //초밥 종류
             m = sc.nextInt() / 100; //예산
             for (int i = 0; i < n; i++) {
-                c1 = sc.nextInt() / 100;
-                c2 = sc.nextInt();
-                sushi.add(new Pair(c1, c2));
+                price[i] = sc.nextInt() / 100;
+                prefer[i] = sc.nextInt();
             }
             System.out.println(DP());
-            sushi.clear();
         }
     }
 }
