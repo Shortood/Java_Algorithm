ea713f1983df4af4f5fe24d8f7454655fced04c4
Kang Woobin <woobin929@dgu.ac.kr>
2024-01-21 23:40:08 +0900

    baekjoon_11726

---
+++ baekjoon_11726/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int N;
+        int[] dp;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+        dp[1] = 1;
+        dp[0] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = ((dp[i - 1] % 10007) + (dp[i - 2] % 10007)) % 10007;
+        }
+        
+        System.out.println(dp[N]);
+
+    }
+}
