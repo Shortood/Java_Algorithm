3119a61995e2aa37db9310a9811e8e973b589b2b
Kang Woobin <woobin929@dgu.ac.kr>
2024-01-28 23:50:29 +0900

    :fix

--- baekjoon/baekjoon_20057.java
+++ baekjoon/baekjoon_20057.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+class Main {
     static int[][] map;
     static int result, x, y;
     static int N, mid;
--- baekjoon/baekjoon_23288.java
+++ baekjoon/baekjoon_23288.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
+class Main {
     static int N, M, K, result = 0;
     static int x = 0, y = 0;
     static int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
