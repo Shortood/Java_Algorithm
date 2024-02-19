0cc8b7c2bbff7b4a6949f83f7f90581c7969cf87
Kang Woobin <woobin929@dgu.ac.kr>
2024-02-20 02:40:58 +0900

    baekjoon_1920

---
+++ baekjoon_1920/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if (Arrays.binarySearch(A, input) < 0)
+                sb.append(0).append('\n');
+            else
+                sb.append(1).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
