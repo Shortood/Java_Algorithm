1fcd1046fda9d4bceb54d401fc7bd50045f9593b
woobin Kang <112168747+Shortood@users.noreply.github.com>
2025-02-06 23:34:44 +0900

    [level 1] Title: 모의고사, Time: 5.46 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] first = {1, 2, 3, 4, 5},
+        second = {2, 1, 2, 3, 2, 4, 2, 5},
+        third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int [] sol = {0, 0, 0};
+        
+        for(int i = 0;i < answers.length;i++){
+            if(answers[i]==first[i%5]) sol[0]++;
+            if(answers[i]==second[i%8]) sol[1]++;
+            if(answers[i]==third[i%10]) sol[2]++;
+        }
+        
+        int max = Math.max(sol[0], sol[1]);
+        max = Math.max(max, sol[2]);
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<3;i++){
+            if(sol[i]>=max) list.add(i+1);
+        }
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        
+        return answer;
+    }
+}
