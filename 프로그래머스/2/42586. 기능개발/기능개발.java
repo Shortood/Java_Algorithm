1d2ab65ee69520507fdabe0d5e51ec7bf49dc293
woobin Kang <112168747+Shortood@users.noreply.github.com>
2025-02-10 00:11:30 +0900

    [level 2] Title: 기능개발, Time: 3.11 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -6,26 +6,28 @@ class Solution {
         int[] answer = {};
         int[] remain = new int[progresses.length];
         int cnt = 1;
+        int maxDay = -1;
+        
         List<Integer> list = new LinkedList<>();
         
         for(int i=0;i<progresses.length;i++){
             remain[i]=(100-progresses[i])/speeds[i];
-            if((100-progresses[i])%speeds[i]>0)
+            
+            if((100-progresses[i])%speeds[i] > 0)
                 remain[i]++;
-            System.out.println(remain[i]);
-        }
-
-        int maxDay=remain[0];
-        
-        for(int i=1;i<remain.length;i++){
-            if(remain[i]<=maxDay){
+            
+            if(remain[i] <= maxDay)
                 cnt++;
-            } else { 
-                list.add(cnt);
+            else {
+                if(maxDay!=-1)
+                    list.add(cnt);
                 cnt = 1;
                 maxDay = remain[i];
             }
+            //System.out.println(remain[i]);
         }
+
+    
         
         list.add(cnt);
         
