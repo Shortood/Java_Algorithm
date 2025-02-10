f942270d1ab9716495f39dd3bacb840986305294
woobin Kang <112168747+Shortood@users.noreply.github.com>
2025-02-11 00:03:24 +0900

    [level 2] Title: 가장 큰 수, Time: 191.86 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] stringNumbers = new String[numbers.length];
+        
+        for(int i=0;i<numbers.length;i++){
+            stringNumbers[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(stringNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2);
+            }
+        });
+        
+        StringBuffer sb = new StringBuffer();
+        
+        for(String string: stringNumbers){
+            sb.append(string);
+        }
+        
+        String result = sb.toString();
+        System.out.println(result.length());
+       while(result.charAt(0) =='0' && result.length() > 1) {
+           result = result.substring(1);
+       }
+        
+        
+        
+        return result;
+    }
+}
