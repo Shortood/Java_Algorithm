1051b95f8e9bf7153cb861c814ce0294ed359d9e
woobin Kang <112168747+Shortood@users.noreply.github.com>
2025-02-28 23:07:07 +0900

    [level 2] Title: 피로도, Time: 2.91 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class Solution {
+    static int N, answer;
+    static boolean[] check;
+    
+    public int solution(int k, int[][] dungeons) {
+        answer = -1;
+        N = dungeons.length;
+        check = new boolean[N];
+        
+        for(int i=0;i < N;i++){
+            if(dungeons[i][0]<=k){
+                check[i]=true;
+                dfs(1, dungeons, k-dungeons[i][1], 1);
+                check[i]=false;
+            }
+        }
+        return answer;
+    }
+    
+    static void dfs(int depth, int[][] dungeons, int nowK, int cnt){
+        //System.out.println(depth + " " + nowK + " " + cnt);
+        answer = Math.max(answer, cnt);
+        
+        for(int i=0;i<N;i++) {
+            if(!check[i]&&nowK>=dungeons[i][0]) {
+                check[i]=true;
+                dfs(depth + 1, dungeons, nowK-dungeons[i][1], cnt + 1);
+                check[i]=false;
+            }
+        }
+    }
+}
